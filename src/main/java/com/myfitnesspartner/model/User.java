f25be94aa6a3320cb1dbb378cdbd36f6baa601c3
Vianney Accart <vianneyaccart@gmail.com>
2022-03-16 19:27:17 +0100

    initial commit

---
+++ src/main/java/com/myfitnesspartner/model/User.java
@@ -0,0 +1,45 @@
+package com.myfitnesspartner.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	@Size(min = 2, max = 50)
+	private String firstname;
+
+	@NotBlank
+	@Size(min = 1, max = 50)
+	private String lastname;
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+}

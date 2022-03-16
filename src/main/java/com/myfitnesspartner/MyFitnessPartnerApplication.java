86282f0829354b31ad2ec965f76b58ac6e0fee86
Vianney Accart <vianneyaccart@gmail.com>
2022-03-16 18:59:44 +0100

    initial commit

---
+++ src/main/java/com/myfitnesspartner/MyFitnessPartnerApplication.java
@@ -0,0 +1,13 @@
+package com.myfitnesspartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitnessPartnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFitnessPartnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myfitnesspartner/MyFitnessPartnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myfitnesspartner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitnessPartnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
